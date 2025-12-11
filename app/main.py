7de45e1b2f93c081d4e8ba93cae8584f5748d1a1
NovatrixSolutions <novatrixsolutions09@gmail.com>
2025-12-11 23:35:51 +0530

    Update main.py
    crocs

--- app/main.py
+++ app/main.py
@@ -23,6 +23,7 @@ app = FastAPI(title="Alumni Recommendation Backend")
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://mentorbridge-frontend.onrender.com"
 ]
 
 app.add_middleware(
